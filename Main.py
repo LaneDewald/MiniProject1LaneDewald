6d21c37be0089239909355b55c93ff7ff7a1cf6f
Lane <l_dewald3@mail.fhsu.edu>
2025-02-17 20:14:43 -0600

    Imported pprint and yfinance Added list of Tickers that will be used in project, then created for loop to look at the closing price over the last 10 open days.

--- Main.py
+++ Main.py
@@ -1,3 +1,18 @@
 ### INF601 - Advanced Programming in Python
 ### Lane Dewald
 ### Mini Project 1
+
+import pprint
+import yfinance as yf
+
+
+tickers = ["AAPL", "GOOGL", "MSFT", "AMZN", "TSLA"]
+days = 10
+
+stock_data = {}
+for ticker in tickers:
+    ticker = yf.Ticker(ticker)
+    print(ticker.history(period=f"{days}d"))
+    stock_data[ticker] = hist["Close"].tolist()
+
+pprint.pprint(stock_data)
