80541fa18ad70ef1dc9d682d1eb1a5195e1fce5c
Lane <l_dewald3@mail.fhsu.edu>
2025-02-17 21:32:31 -0600

    Added code to create Charts folder if it does not exist

--- Main.py
+++ Main.py
@@ -6,7 +6,9 @@ import pprint
 import yfinance as yf
 import numpy as np
 import matplotlib.pyplot as plt
+import os
 
+os.makedirs("Charts", exist_ok=True)
 
 tickers = ["AAPL", "GOOGL", "MSFT", "AMZN", "TSLA"]
 
